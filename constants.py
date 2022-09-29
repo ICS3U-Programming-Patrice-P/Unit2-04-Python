39d754e9b02c78fe7a299e9b62e943354999976a
Patrice Pat-Odita <patrice.pat-odita@stu.ocsb.ca>
2022-09-29 00:00:59 +0000

    Added pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Patrice Pat-Odita
+# Created on: Sep 28, 2022
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGREDIENTS_COST = 1.50
---
+++ pizza_cost.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Created by: Patrice Pat-Odita
+# Created on: Sep 28, 2022
+# This program asks the user for the diameter of the
+# pizza. It then calculates and displays the total cost
+# of the pizza with tax
+import constants
+
+
+def main():
+    # input
+    diameter = int(input("Enter the diameter of the pizza (inches): "))
+
+    # process
+    subtotal = (
+        constants.LABOUR_COST
+        + constants.RENTAL_COST
+        + constants.INGREDIENTS_COST * diameter
+    )
+    tax = constants.HST * subtotal
+    total = subtotal + tax
+
+    # output
+    print("")
+    print("The total cost is = ${:,.2f}".format(total))
+
+
+if __name__ == "__main__":
+    main()
